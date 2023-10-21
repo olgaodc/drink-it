1e99767b7328837359c8a2e581ded1f800ef1ae5
olgaodc <olgaodincova@gmail.com>
2023-10-22 01:44:08 +0300

    Home page responsive design added

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Navbar.module.css';
 import Link from 'next/link';
 import Container from '../Container/Container';
 
+const DesktopMenu = () => {
+  return (
+    <ul className={styles.navbarList}>
+      <li className={styles.listItem}>
+        <Link className={styles.itemLink} href={'/'}>
+          Home
+        </Link>
+      </li>
+      <li className={styles.listItem}>
+        <Link className={styles.itemLink} href={'/cocktails'}>
+          Cocktails
+        </Link>
+      </li>
+      <li className={styles.listItem}>
+        <Link className={styles.itemLink} href={'/random-cocktail'}>
+          Random cocktail
+        </Link>
+      </li>
+    </ul>
+  )
+}
+
+const MobileMenu = (props: { isMenuOpen: boolean; }) => {
+  return (
+    <div className={`${styles.mobileMenuWrapper} ${props.isMenuOpen ? styles.menuOpened : styles.menuClosed}`}>
+      <ul className={styles.mobileMenu}>
+        <li className={styles.listItem}>
+          <Link className={styles.itemLink} href={'/'}>
+            Home
+          </Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link className={styles.itemLink} href={'/cocktails'}>
+            Cocktails
+          </Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link className={styles.itemLink} href={'/random-cocktail'}>
+            Random cocktail
+          </Link>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
 const Navbar = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
   return (
     <header className={styles.navbarWrapper}>
       <Container>
@@ -14,24 +62,16 @@ const Navbar = () => {
               .it
             </span>
           </Link>
-          <ul className={styles.navbarList}>
-          <li className={styles.listItem}>
-              <Link className={styles.itemLink} href={'/'}>
-                Home
-              </Link>
-            </li>
-            <li className={styles.listItem}>
-              <Link className={styles.itemLink} href={'/cocktails'}>
-                Cocktails
-              </Link>
-            </li>
-            <li className={styles.listItem}>
-              <Link className={styles.itemLink} href={'/random-cocktail'}>
-                Random cocktail
-              </Link>
-            </li>
-          </ul>
+          <DesktopMenu />
+          <div onClick={() => {
+            setMenuOpen((prevState) => !prevState);
+          }} className={styles.hamburgerMenu}>
+            <div className={`${styles.hamburgerLine} ${isMenuOpen ? styles.open : ''}`}></div>
+            <div className={`${styles.hamburgerLine} ${isMenuOpen ? styles.open : ''}`}></div>
+            <div className={`${styles.hamburgerLine} ${isMenuOpen ? styles.open : ''}`}></div>
+          </div>
         </nav>
+        <MobileMenu isMenuOpen={isMenuOpen} />
       </Container>
     </header>
   )
--- src/components/Recommendations/Recommendations.tsx
+++ src/components/Recommendations/Recommendations.tsx
@@ -19,72 +19,58 @@ const Recommendations = () => {
       items: 3
     },
     desktop: {
-      breakpoint: { max: 1300 , min: 952 },
+      breakpoint: { max: 1300, min: 952 },
       items: 3
     },
     tablet: {
-      breakpoint: { max: 952, min: 600 },
+      breakpoint: { max: 952, min: 756 },
       items: 2
     },
     mobile: {
-      breakpoint: { max: 600, min: 0 },
+      breakpoint: { max: 756, min: 537 },
+      items: 1.7
+    },
+    mobile2: {
+      breakpoint: { max: 537, min: 0 },
       items: 1
     }
   };
 
   return (
-    <Container>
-      <div className={styles.recommendationsWrapper}>
-        <div className={styles.recommendations}>
-          <h3 className={styles.title}>We recommended</h3>
-          <article className={styles.description}>We all love cheeky cocktail, who doesn't? Whether it's sweet or sour, gin or vodka, we have a cocktail to suit you. Get ahead of the crowd out what we have to offer and find you new favorite. Call it "research", we won't tell anyone...</article>
-          <div>
-            <Carousel
-              responsive={responsive}
-              swipeable={true}
-              keyBoardControl={true}
-            >
-              <Card cover={<img alt="example" src={CardImage1.src} />}>
-                <Meta title="Mojito" />
-              </Card>
-              <Card cover={<img alt="example" src={CardImage3.src} />} >
-                <Meta title="Margarita" />
-              </Card>
-              <Card cover={<img alt="example" src={CardImage2.src} />} >
-                <Meta title="Negroni" />
-              </Card>
-              <Card cover={<img alt="example" src={CardImage4.src} />} >
-                <Meta title="Salty Dog" />
-              </Card>
-              <Card cover={<img alt="example" src={CardImage5.src} />} >
-                <Meta title="Vodka Lemon" />
-              </Card>
-            </Carousel>
+    <div className={styles.recommendationsContainer}>
+      <Container>
+        <div className={styles.recommendationsWrapper}>
+          <div className={styles.recommendations}>
+            <h3 className={styles.title}>We recommended</h3>
+            <article className={styles.description}>We all love cheeky cocktail, who doesn't? Whether it's sweet or sour, gin or vodka, we have a cocktail to suit you. Get ahead of the crowd out what we have to offer and find you new favorite. Call it "research", we won't tell anyone...</article>
           </div>
         </div>
-      </div>
-      {/* <Carousel
-        responsive={responsive}
-        swipeable={true}
-        keyBoardControl={true}
-      >
-        <Card cover={<img alt="example" src={CardImage1.src} />}>
-          <Meta title="Mojito" />
-        </Card>
-        <Card cover={<img alt="example" src={CardImage3.src} />} >
-          <Meta title="Margarita" />
-        </Card>
-        <Card cover={<img alt="example" src={CardImage2.src} />} >
-          <Meta title="Negroni" />
-        </Card>
-        <Card cover={<img alt="example" src={CardImage4.src} />} >
-          <Meta title="Salty Dog" />
-        </Card>
-        <Card cover={<img alt="example" src={CardImage5.src} />} >
-          <Meta title="Vodka Lemon" />
-        </Card>
-      </Carousel> */}
-    </Container>
+        <div className={styles.carouselWrapper}>
+          <Carousel
+            responsive={responsive}
+            swipeable={true}
+            keyBoardControl={true}
+          >
+            <Card cover={<img alt="example" src={CardImage1.src} />}>
+              <Meta title="Mojito" />
+            </Card>
+            <Card cover={<img alt="example" src={CardImage3.src} />} >
+              <Meta title="Margarita" />
+            </Card>
+            <Card cover={<img alt="example" src={CardImage2.src} />} >
+              <Meta title="Negroni" />
+            </Card>
+            <Card cover={<img alt="example" src={CardImage4.src} />} >
+              <Meta title="Salty Dog" />
+            </Card>
+            <Card cover={<img alt="example" src={CardImage5.src} />} >
+              <Meta title="Vodka Lemon" />
+            </Card>
+          </Carousel>
+        </div>
+      </Container>
+    </div>
+
   )
 }
 
