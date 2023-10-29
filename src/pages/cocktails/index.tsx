6893ba18e7b93f49c698a4e5632180f43de96804
olgaodc <olgaodincova@gmail.com>
2023-10-30 00:08:52 +0200

    Cocktails page updated: Background image added; search bar changed: search by cocktail name and letter added; pagination added.

--- src/pages/cocktails/index.tsx
+++ src/pages/cocktails/index.tsx
@@ -5,6 +5,8 @@ import Container from "@/components/Container/Container";
 import axios from "axios";
 import SmallCard from "@/components/SmallCard/SmallCard";
 import Footer from "@/components/Footer/Footer";
+import { Input, Pagination, Select } from "antd";
+import PrimaryButton from "@/components/Button/Button";
 
 type CocktailProps = {
   idDrink: string,
@@ -19,6 +21,14 @@ const CocktailsPage = () => {
   const [cocktails, setCocktails] = useState<CocktailsProps>();
   // const [displayedCocktails, setDisplayedCocktails] = useState<CocktailsProps>();
   const [inputText, setInputText] = useState('');
+  const [searchOption, setSearchOption] = useState('ingredient'); 
+  const [page, setPage] = useState(1);
+  const pageSize = 20;
+
+
+  useEffect(() => {
+    fetchCocktailsByIngredient('ice');
+  }, []);
 
   const fetchCocktailsByIngredient = async (inputText: string) => {
     try {
@@ -27,67 +37,116 @@ const CocktailsPage = () => {
       const { drinks } = response.data;
 
       setCocktails(drinks);
-      // setDisplayedCocktails(drinks);
     } catch (err) {
       console.log(err)
     }
   };
 
-  useEffect(() => {
-    fetchCocktailsByIngredient('ice');
-  }, []);
 
-  // const fetchCocktailsByFirstLetter = async (inputText: string) => {
-  //   const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${inputText}`);
-  //   const data = await response.json();
-  //   setDisplayedCocktails(data.drinks)
-  //   console.log(data.drinks)
-  // }
+  const fetchCocktailsByName = async (inputText: string) => {
+    try {
+      const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputText}`);
+
+      const { drinks } = response.data;
+
+      setCocktails(drinks);
+    } catch (err) {
+      console.log(err)
+    }
+  };
+
+
+  const fetchCocktailsByFirstLetter = async (inputText: string) => {
+    try {
+      const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${inputText}`);
+
+      const { drinks } = response.data;
+
+      setCocktails(drinks);
+
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const onSearchOptionChange = (value: string) => {
+    setSearchOption(value);
+  };
+
+    const getInputPlaceholder = () => {
+      if (searchOption === 'ingredient') {
+        return 'e.g gin';
+      } else if (searchOption === 'first-letter') {
+        return 'e.g a';
+      } else if (searchOption === 'name') {
+        return 'e.g Margarita';
+      }
+      return '';
+    };
+  
+    const getInputMaxLength = () => {
+      return searchOption === 'first-letter' ? 1 : 15;
+    };
+
+  const onSearch = () => {
+    if (searchOption === 'ingredient') {
+      fetchCocktailsByIngredient(inputText);
+      setInputText('');
+    } else if (searchOption === 'name') {
+      fetchCocktailsByName(inputText);
+      setInputText('');
+    } else if (searchOption === 'first-letter') {
+      fetchCocktailsByFirstLetter(inputText);
+      setInputText('');
+    } 
+  };
+
+  const onPageChange = (currentPage: number) => {
+    setPage(currentPage);
+  };
 
   return (
     <>
-      <Navbar />
+      <div className={styles.bgImage} >
+        <Navbar />
+      </div>
+      <Container>
+        <div className={styles.buttonsWrapper}>
+          <div className={styles.searchTitle}>Search by</div>
+          <Select
+            className={styles.cocktailSelect}
+            defaultValue="Ingredient"
+            onChange={onSearchOptionChange}
+            options={[
+              { value: 'ingredient', label: 'Ingredient' },
+              { value: 'name', label: 'Cocktail name' },
+              { value: 'first-letter', label: 'Cocktail first letter' },
+              // { value: 'disabled', label: 'Disabled', disabled: true },
+            ]}
+          />
+
+          <Input
+            className={styles.cocktailInput}
+            onChange={(event) => setInputText(event.target.value)}
+            value={inputText}
+            placeholder={getInputPlaceholder()}
+            maxLength={getInputMaxLength()}
+          />
+
+          <PrimaryButton
+            // className={styles.findCocktailButton}
+            onClick={onSearch}
+          >
+            Search
+          </PrimaryButton>
+        </div>
+      </Container>
+
       <div className={styles.content}>
         <Container>
-          <div className={styles.buttonsWrapper}>
-            {/* <button
-              onClick={() => setDisplayedCocktails(() => cocktails?.filter((cocktail) => cocktail.strAlcoholic === 'Alcoholic'))}
-            >
-              Alcoholic
-            </button>
-
-            <button
-              onClick={() => setDisplayedCocktails(() => cocktails?.filter((cocktail) => cocktail.strAlcoholic !== 'Alcoholic'))}
-            >
-              Non Alcoholic
-            </button>
-
-            <button
-              onClick={() => setDisplayedCocktails(cocktails)}
-            >
-              All Cocktails
-            </button> */}
-
-            <input
-              className={styles.findCocktailInput}
-              onChange={(event) => setInputText(event.target.value)}
-              value={inputText}
-              placeholder="E.g Gin"
-            />
-
-            <button
-              className={styles.findCocktailButton}
-              onClick={() => {
-                fetchCocktailsByIngredient(inputText);
-                setInputText('');
-              }}
-            >
-              Find
-            </button>
-          </div>
           <div className={styles.cocktailsSectionWrapper}>
             <div className={styles.cocktailsSection}>
-              {cocktails && cocktails.map((cocktail) => (
+              {cocktails && cocktails.slice((page - 1) * pageSize, page * pageSize).map((cocktail) => (
                 <SmallCard
                   key={cocktail.idDrink}
                   id={cocktail.idDrink}
@@ -95,6 +154,16 @@ const CocktailsPage = () => {
                   photoUrl={cocktail.strDrinkThumb}
                 />
               ))}
+            </div>
+            <div className={styles.paginationWrapper}>
+              <Pagination
+                className={styles.pagination}
+                current={page}
+                pageSize={pageSize}
+                total={cocktails?.length}
+                onChange={onPageChange}
+                showSizeChanger={false}
+              />
             </div>
           </div>
         </Container>
